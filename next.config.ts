cf5ffc93cbfbbdea5c93f60e06c919c2539bc199
Zeeshan Ahmad <zeeshanahmaddeb@yahoo.com>
2024-12-14 06:31:14 +0100

    Complete admin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/FitGeek_Admin',  // This is the repo name
+  assetPrefix: '/FitGeek_Admin/',  // This ensures that static assets are correctly loaded
+  trailingSlash: true,  // Optional: Adds a trailing slash to URLs
 };
 
 export default nextConfig;
